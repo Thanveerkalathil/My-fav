44e045404d5c86cf7bba2023d2bf6e41ccf7c60f
Thanveerkalathil <mthanveermltr@gmail.com>
2023-01-13 20:02:47 +0530

    home story view react complite

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,16 +1,22 @@
 import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import CreatePost from "../../components/createPost";
 import Header from "../../components/header";
 import LeftHome from "../../components/home/left";
 import RightHome from "../../components/home/right";
-
+import Stories from "../../components/home/stories";
+import "./style.css";
 export default function Home() {
   const { user } = useSelector((user) => ({ ...user }));
   return (
-    <div>
+    <div className="home">
       <Header />
       <LeftHome user={user} />
-      <RightHome user={user}/>
+      <div className="home_middle">
+        <Stories />
+        <CreatePost user={user}/>
+      </div>
+      <RightHome user={user} />
     </div>
   );
 }
