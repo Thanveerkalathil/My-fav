2e757add0ce68614556c1e5ccb580ee1e3d861be
Mohammed Thanveer <103193237+Thanveerkalathil@users.noreply.github.com>
2023-09-06 10:49:37 +0530

    Display all posts

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createPost } = require("../controller/post");
+const { createPost, getAllPosts } = require("../controller/post");
 const { authUser } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.post("/createPost", authUser, createPost);
+router.get("/getAllPosts", authUser, getAllPosts);
 
 module.exports = router;
