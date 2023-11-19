3ca6abcdcbf03098827fd6bf470472e446b4b4e1
Mohammed Thanveer <mthanveermltr@gmail.com>
2023-11-19 08:09:26 +0530

    Friends request page Buttons functionalities

--- frontend/src/pages/friends/Card.js
+++ frontend/src/pages/friends/Card.js
@@ -1,21 +1,57 @@
 import { Link } from "react-router-dom";
+import {
+  acceptRequest,
+  cancelRequest,
+  deleteRequest,
+} from "../../functions/user";
+import { useSelector } from "react-redux";
 
-export default function Card({ user, type }) {
-  console.log(user);
+export default function Card({ userr, type, getData }) {
+  const { user } = useSelector((state) => ({ ...state }));
+  const cancelRequestHandler = async (userId) => {
+    const res = await cancelRequest(userId, user.token);
+    if (res == "ok") {
+      getData();
+    }
+  };
+  const confirmHandler = async (userId) => {
+    const res = await acceptRequest(userId, user.token);
+    if (res == "ok") {
+      getData();
+    }
+  };
+  const deleteHandler = async (userId) => {
+    const res = await deleteRequest(userId, user.token);
+    if (res == "ok") {
+      getData();
+    }
+  };
   return (
     <div className="req_card">
-      <Link to={`/profile/${user.username}`}>
-        <img src={user.picture} alt="" />
+      <Link to={`/profile/${userr.username}`}>
+        <img src={userr.picture} alt="" />
       </Link>
       <div className="req_name">
-        {user.first_name} {user.last_name}
+        {userr.first_name} {userr.last_name}
       </div>
       {type === "sent" ? (
-        <button className="fav_button">Cancel Request</button>
+        <button
+          className="fav_button"
+          onClick={() => cancelRequestHandler(userr._id)}
+        >
+          Cancel Request
+        </button>
       ) : type === "request" ? (
         <>
-          <button className="fav_button">Confirm</button>
-          <button className="gray_btn">Delete</button>
+          <button
+            className="fav_button"
+            onClick={() => confirmHandler(userr._id)}
+          >
+            Confirm
+          </button>
+          <button className="gray_btn" onClick={() => deleteHandler(userr._id)}>
+            Delete
+          </button>
         </>
       ) : (
         ""
--- frontend/src/pages/friends/index.js
+++ frontend/src/pages/friends/index.js
@@ -108,7 +108,12 @@ export default function Friends() {
             <div className="flex_wrap">
               {data.requests &&
                 data.requests.map((user) => (
-                  <Card user={user} key={user._id} type="request" />
+                  <Card
+                    userr={user}
+                    key={user._id}
+                    type="request"
+                    getData={getData}
+                  />
                 ))}
             </div>
           </div>
@@ -120,7 +125,12 @@ export default function Friends() {
             <div className="flex_wrap">
               {data.sentRequests &&
                 data.sentRequests.map((user) => (
-                  <Card user={user} key={user._id} type="sent" />
+                  <Card
+                    userr={user}
+                    key={user._id}
+                    type="sent"
+                    getData={getData}
+                  />
                 ))}
             </div>
           </div>
@@ -132,7 +142,12 @@ export default function Friends() {
             <div className="flex_wrap">
               {data.friends &&
                 data.friends.map((user) => (
-                  <Card user={user} key={user._id} type="friends" />
+                  <Card
+                    userr={user}
+                    key={user._id}
+                    type="friends"
+                    getData={getData}
+                  />
                 ))}
             </div>
           </div>
